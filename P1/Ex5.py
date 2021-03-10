207221273c6e1f9643b4238c36e81da00edc84ab
amate <a.mateosm.2020@alumnos.urjc.es>
2021-03-10 17:43:21 +0100

    improvement Ex5 on printing result

--- P1/Ex5.py
+++ P1/Ex5.py
@@ -1,17 +1,18 @@
 from Seq1 import Seq
+
+
+def print_result(seq_list):
+    nucleobases_dna = ["A", "C", "T", "G"]
+    for sequence in seq_list:
+        print(f"Sequence {seq_list.index(sequence)}: (Length: {sequence.len()}) {sequence}")
+        for base in nucleobases_dna:
+            print(f"   {base}: {sequence.count_base(base)},", end="")
+        print()
+
+
 print("-----| Practice 1, Exercise 5 |------")
-# -- Create a Null sequence
 s1 = Seq()
-
-# -- Create a valid sequence
 s2 = Seq("ACTGA")
-
-# -- Create an invalid sequence
 s3 = Seq("Invalid sequence")
-seq_list = [s1, s2, s3]
-nucleobases_dna = ["A", "C", "T", "G"]
-for sequence in seq_list:
-    print(f"Sequence {seq_list.index(sequence)}: (Length: {sequence.len()}) {sequence}")
-    for base in nucleobases_dna:
-        print(f"   {base}: {sequence.count_base(base)},", end="")
-    print()
+list_seq = [s1, s2, s3]
+print_result(list_seq)
